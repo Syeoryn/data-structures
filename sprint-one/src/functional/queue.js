21a4653833e122755a3c0cf3db62fbc5db93fefe
Hack Reactor Students <syeoryn@gmail.com>
2014-04-16 15:24:27 -0700

    Passes all queue tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,31 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var first = 0;
+  var last = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[last] = value;
+    last++;
   };
 
   instance.dequeue = function(){
+    if(last -first >0){
+      var  removed = storage[first];
+      delete storage[first];
+      first++;
+      return removed;
+    }
+
   };
 
   instance.size = function(){
+    return last - first;
   };
 
   return instance;
 };
+
+
