a64ac7e6133af2e931a57913da7e877e8af5841a
Hack Reactor Students <syeoryn@gmail.com>
2014-04-17 10:22:44 -0700

    Passes pseudoclassical stack tests.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,22 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+
+  // Use an object with numeric keys to store values
+  this.storage = {};
+  this.length = 0; // Hint: set an initial value here
+};
+
+Stack.prototype.size = function(){
+  return this.length;
+};
+Stack.prototype.push = function(value){
+  this.storage[this.length] = value;
+  this.length++;
+};
+Stack.prototype.pop =  function(){
+  if(this.length){
+    this.length--;
+    var removed = this.storage[this.length];
+    delete this.storage[this.length];
+    return removed;
+  }
 };
