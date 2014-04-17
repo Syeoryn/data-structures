44df3292f44eaefe4b43adefe25796bfd8b44d5f
Hack Reactor Students <syeoryn@gmail.com>
2014-04-17 16:26:01 -0700

    Passes linkedList tests.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,29 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if(!this.head){
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
   };
 
   list.removeHead = function(){
+    var removed = this.head.value;
+    this.head = this.head.next;
+    return removed;
   };
 
   list.contains = function(target, node){
+    node = node || this.head;
+    if(node.value === target) {
+      return true;
+    } else if(node.next){
+      return this.contains(target,node.next);
+    }
+    return false;
   };
 
   return list;
