69016797dd5c93d560d48f328bd974f76d81a32e
Hack Reactor Students <syeoryn@gmail.com>
2014-04-17 20:58:45 -0700

    Passes tree tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,6 +2,7 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  extend(newTree,treeMethods);
   return newTree;
 };
 
@@ -9,8 +10,30 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var newNodeTree = makeTree(value);
+  if(!this.children){
+    this.children = [];
+  }
+  this.children.push(newNodeTree);
 };
 
 treeMethods.contains = function(target){
+  if(this.value === target){
+    return true;
+  } else if(this.children){
+    for(var i = 0; i < this.children.length; i++){
+      if(this.children[i].contains(target)){
+        return true;
+      };
+    }
+  }
+  return false;
 };
 
+function extend(to,from){
+  for(key in from){
+    if(!(key in to)){
+      to[key]= from[key];
+    }
+  }
+}
