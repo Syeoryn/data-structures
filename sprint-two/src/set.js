a5c86ddfb54b962d4ced7f2dbc4687122ea218c6
Hack Reactor Students <syeoryn@gmail.com>
2014-04-18 11:35:23 -0700

    Passes set tests.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,10 +7,19 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(!this._storage){
+    this._storage = {};
+  }
+  var key = JSON.stringify(item);
+  this._storage[key] = item;
 };
 
 setPrototype.contains = function(item){
+  var key = JSON.stringify(item);
+  return(this._storage[key] === item);
 };
 
 setPrototype.remove = function(item){
+  var key = JSON.stringify(item);
+  return delete this._storage[key];
 };
