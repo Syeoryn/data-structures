39a1bb4b74e2442e40d7f1ce9f1d792f066e3771
Hack Reactor Students <syeoryn@gmail.com>
2014-04-18 21:10:54 -0700

    Passes hashTable tests.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,13 +4,35 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var holder = this._storage.get(index) || [];
+  holder.push([k,v]);
+  this._storage.set(index,holder);
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var holder = this._storage.get(index);
+  if( holder !== undefined) {
+    for(var i = 0; i < holder.length; i++){
+      if(holder[i][0] === k){
+        return holder[i][1];
+      }
+    }
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k){
+  var index = getIndexBelowMaxForKey(k,this._limit);
+  var holder = this._storage.get(index);
+  if( holder !== undefined){
+    for(var i = 0; i < holder.length; i++){
+      if(holder[i][0] ===k){
+        holder.splice(i,1);
+      }
+    }
+    this._storage.set(index,holder);
+  }
 };
 
